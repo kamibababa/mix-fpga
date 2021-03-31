5b5cc50b6e0ea33959bad7a56c67d73a26a57769
Michael Schröder <mi.schroeder@netcologne.de>
2021-03-31 15:00:23 +0200

    added NOP

--- mix/mix.v
+++ mix/mix.v
@@ -20,18 +20,22 @@ module mix(
 	reg [12:0] RegisterI6;
 	reg [11:0] RegisterJ;
 	
-	//cycle
-	reg cycle;
+	//fetch
+	reg fetch;
 	always @(posedge clk)
-		if (reset) cycle <= 0;
-		else cycle <= ~cycle;
+		if (reset) fetch <= 0;
+		else if (nop) fetch <= 1;
+		else fetch <= ~fetch;
 
 	//programm counter
 	reg [11:0] pc;
 	always @(posedge clk)
 		if (reset) pc <= 0;
-		else if (cycle) pc <= pc+1;
+		else if (nop) pc <= pc+1;
+		else if (~fetch) pc <= pc+1;
 	
+	wire nop;
+	assign nop = fetch & (command == 6'd0);
 
 	//memory cells
 	reg [30:0] memory[0:4095];
@@ -41,90 +45,91 @@ module mix(
 	end
 	reg [30:0] data;
 	wire [11:0] address;
-		assign address = (cycle==1'b0)? pc : data[29:18];
+		assign address = ((fetch==1'b0)|nop)? pc : data[29:18];
 	always @(posedge clk)
 		data <= memory[address];
 
 	//COMMAND
 	wire [5:0] command;
 	assign command = data[5:0];
+	
 	//LDA,LDAN
 	reg loadA;
 	always @(posedge clk)
-		loadA <= cycle & (command==6'd8);
+		loadA <= fetch & (command==6'd8);
 	reg loadAN;
 	always @(posedge clk)
-		loadAN <= cycle & (command==6'd16);
+		loadAN <= fetch & (command==6'd16);
 	always @(posedge clk)
 		if (loadA) RegisterA <= fieldLoad;
 		else if (loadAN) RegisterA <= {~fieldLoad[30],fieldLoad[29:0]};
 	//LD1,LD1N
 	reg load1;
 	always @(posedge clk)
-		load1 <= cycle & (command==6'd9);
+		load1 <= fetch & (command==6'd9);
 	reg load1N;
 	always @(posedge clk)
-		load1N <= cycle & (command==6'd17);
+		load1N <= fetch & (command==6'd17);
 	always @(posedge clk)
 		if (load1) RegisterI1 <= {fieldLoad[30],fieldLoad[11:0]};
 		else if (load1N) RegisterI1 <= {~fieldLoad[30],fieldLoad[11:0]};
 	//LD2
 	reg load2;
 	always @(posedge clk)
-		load2 <= cycle & (command==6'd10);
+		load2 <= fetch & (command==6'd10);
 	reg load2N;
 	always @(posedge clk)
-		load2N <= cycle & (command==6'd18);
+		load2N <= fetch & (command==6'd18);
 	always @(posedge clk)
 		if (load2) RegisterI2 <= {fieldLoad[30],fieldLoad[11:0]};
 		else if (load2N) RegisterI2 <= {~fieldLoad[30],fieldLoad[11:0]};
 	//LD3
 	reg load3;
 	always @(posedge clk)
-		load3 <= cycle & (command==6'd11);
+		load3 <= fetch & (command==6'd11);
 	reg load3N;
 	always @(posedge clk)
-		load3N <= cycle & (command==6'd19);
+		load3N <= fetch & (command==6'd19);
 	always @(posedge clk)
 		if (load3) RegisterI3 <= {fieldLoad[30],fieldLoad[11:0]};
 		else if (load3N) RegisterI3 <= {~fieldLoad[30],fieldLoad[11:0]};
 	//LD4
 	reg load4;
 	always @(posedge clk)
-		load4 <= cycle & (command==6'd12);
+		load4 <= fetch & (command==6'd12);
 	reg load4N;
 	always @(posedge clk)
-		load4N <= cycle & (command==6'd20);
+		load4N <= fetch & (command==6'd20);
 	always @(posedge clk)
 		if (load4) RegisterI4 <= {fieldLoad[30],fieldLoad[11:0]};
 		else if (load4N) RegisterI4 <= {~fieldLoad[30],fieldLoad[11:0]};
 	//LD5
 	reg load5;
 	always @(posedge clk)
-		load5 <= cycle & (command==6'd13);
+		load5 <= fetch & (command==6'd13);
 	reg load5N;
 	always @(posedge clk)
-		load5N <= cycle & (command==6'd21);
+		load5N <= fetch & (command==6'd21);
 	always @(posedge clk)
 		if (load5) RegisterI5 <= {fieldLoad[30],fieldLoad[11:0]};
 		else if (load5N) RegisterI5 <= {~fieldLoad[30],fieldLoad[11:0]};
 	//LD6
 	reg load6;
 	always @(posedge clk)
-		load6 <= cycle & (command==6'd14);
+		load6 <= fetch & (command==6'd14);
 	reg load6N;
 	always @(posedge clk)
-		load6N <= cycle & (command==6'd22);
+		load6N <= fetch & (command==6'd22);
 	always @(posedge clk)
 		if (load6) RegisterI6 <= {fieldLoad[30],fieldLoad[11:0]};
 		else if (load6N) RegisterI6 <= {~fieldLoad[30],fieldLoad[11:0]};
 	//LDX
 	reg loadX;
 	always @(posedge clk)
-		loadX <= cycle & (command==6'd15);
+		loadX <= fetch & (command==6'd15);
 	reg loadXN;
 	always @(posedge clk)
-		loadXN <= cycle & (command==6'd23);
+		loadXN <= fetch & (command==6'd23);
 	always @(posedge clk)
 		if (loadX) RegisterX <= fieldLoad;
 		else if (loadXN) RegisterX <= {~fieldLoad[30],fieldLoad[29:0]};
@@ -132,10 +137,10 @@ module mix(
 	//ST
 	reg store;
 	always @(posedge clk)
-		store <=  cycle & (command[5:3]==3'b011);
+		store <=  fetch & (command[5:3]==3'b011);
 	reg [30:0] dataS;
 	always @(posedge clk)
-		if (cycle & (command[5:3] == 3'b011))
+		if (fetch & (command[5:3] == 3'b011))
 			dataS <= (command[2]?
 					(command[1]?
 						(command[0]?
@@ -157,7 +162,7 @@ module mix(
 	field FIELDM(.in(data),.field(f),.out(fieldLoad));
 	reg [5:0] f;
 	always @(posedge clk)
-		if (cycle) f <= data[11:6];
+		if (fetch) f <= data[11:6];
 	reg [11:0] addressStore;
 	always @(posedge clk)
 		addressStore <= data[29:18];	
