312b9179f32fc826229a27d9ba68190d62481f2f
Michael Schröder <mi.schroeder@netcologne.de>
2021-05-18 13:27:48 +0200

    memory test U8

--- mixal/disk/randcard.py
+++ mixal/disk/randcard.py
@@ -2,14 +2,16 @@
 import sys
 import random
 c=''
+n = int(input('how many punched cards?'))
+fout = open('rand.card','w')
 def char(i):
     if (i==0):
         return ' '
     if (i<30):
         return str(chr(i+65))
     return ' '
-for i in range(6000):
+for i in range(n):
     c=''
     for j in range(80):
         c=c+char(random.randint(0,24))
-    print(c)
+    print(c,file=fout)
