d761c0491de823a5a13d3904b8a747c37a605534
Michael Schröder <mi.schroeder@netcologne.de>
2021-07-20 19:10:56 +0200

    added -r option cardreader.py

--- mixal/tools/cardreader.py
+++ mixal/tools/cardreader.py
@@ -2,20 +2,28 @@
 import sys
 import os
 import time
-if (len(sys.argv)) == 2:
-    t=0
-elif (len(sys.argv)) == 3:
-    t = float(sys.argv[2])
-else:
-    print("usage: {:s} <asm-file.card> [time]".format(sys.argv[0]))
 
-card_file = sys.argv[1]
-
+t=0
+name = sys.argv.pop(0)
+card_file=None
+cr = ''
+for p in sys.argv:
+    if p[0:2]=='-r':
+        cr='\r'
+    elif p[0:2]=='-t':
+        t=float(p[2::])
+    else:
+        card_file = p
+        
+if card_file==None:
+    print("usage: {:s} <asm-file.card> [-t<time>] [-r]".format(name))
+    sys.exit(-1)
+    
 fin = open(card_file,'r')
 fin = fin.read().split('\n')
 
 fout = open('/dev/ttyUSB0','w')
 for line in fin:
     time.sleep(t)
-    print(line,file = fout,end='\n\r')
+    print(line,file = fout,end=cr)
     fout.flush()
